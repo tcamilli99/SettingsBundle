0bf8825a2ce1b60a13c4893f4af47c906b54bd21
Tony Camilli <tony.camilli@wireframerate.com>
2018-08-22 13:30:55 -0500

    Initial Commit

--- SettingsBundle/AppDelegate.swift
+++ SettingsBundle/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        loadDefaultSettingsFromBundle()
         return true
     }
 
@@ -39,6 +40,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func loadDefaultSettingsFromBundle() {
+        //Read PreferenceSpecifiers from Root.plist in Settings.Bundle
+        if let settingsURL = Bundle.main.url(forResource: "Root", withExtension: "plist", subdirectory: "Settings.bundle"),
+            let settingsPlist = NSDictionary(contentsOf: settingsURL),
+            let preferences = settingsPlist["PreferenceSpecifiers"] as? [NSDictionary] {
+            
+            //UserDefaults.standard.register(defaults: preferences)
+            //UserDefaults.standard.register(defaults: settingsPlist as! [String : Any])
+            
+            
+            for preference in preferences {
+               if let key = preference["Key"] as? String, let value = preference["DefaultValue"] {
+                
+                UserDefaults.standard.register(defaults: [key : value])
+                NSLog("loadDefaultSettingsFromBundle:  Set \(key) default value to \(value)")
+                    
+                    //If key doesn't exists in userDefaults then register it, else keep original value
+                    /*if UserDefaults.standard.value(forKey: key) == nil {
+                        
+                        UserDefaults.standard.set(value, forKey: key)
+                        NSLog("registerDefaultsFromSettingsBundle: Set following to UserDefaults - (key: \(key), value: \(value), type: \(type(of: value)))")
+                    }*/
+                }
+            }
+        } else {
+            NSLog("loadDefaultSettingsFromBundle: Could not find Settings.bundle")
+        }
+        
     }
 
 
--- SettingsBundle/ViewController.swift
+++ SettingsBundle/ViewController.swift
@@ -13,11 +13,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        debugPrint("Settings at viewDidLoad():  ")
+        logCurrentSettings()
+        
+        //Register a notification delegate when settings are changed while app is running
+       NotificationCenter.default.addObserver(self, selector: #selector(userDefaultsDidChange), name: UserDefaults.didChangeNotification, object: nil)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getSettingFromDefaults(Key: String) -> Any? {
+        
+        let userDefaults = UserDefaults.standard
+        let retVal = userDefaults.value(forKey: Key)
+        return retVal
+    }
+    
+    func logCurrentSettings() {
+        debugPrint("********** Settings Dump **********")
+        logValueForKey(Key: "name_preference")
+        logValueForKey(Key: "url_preference")
+        logValueForKey(Key: "email_preference")
+        logValueForKey(Key: "enabled_preference")
+        logValueForKey(Key: "slider_preference")
+        debugPrint("**********************************")        
+    }
+    
+    func logValueForKey(Key: String) {
+        guard let valueForKey = getSettingFromDefaults(Key: Key) else {
+            NSLog("Error getting value for key named \(Key)")
+            return
+        }
+        debugPrint("\(Key)  :  \(valueForKey)")
+    }
+    
+    @objc func userDefaultsDidChange() {
+        debugPrint("Settings Changed")
+        logCurrentSettings()
     }
 
 
